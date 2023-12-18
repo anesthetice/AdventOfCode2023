906734b423d800a219c7c6b85d535ff907330d1e
anesthetice <elliot.currat@epfl.ch>
2023-12-18 10:38:23 +0100

    day9 part 1 and 2 in only 30-ish lines of code

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
     let input: &'static str = include_str!("../input.txt");
-    part_1(input);
-}
 
-fn part_1(input: &str) {
-
-    let output = input
+    let output: (i64, i64) = input
         .lines()
         .map(|line| {
             let mut input: Vec<i64> = line
@@ -13,21 +9,27 @@ fn part_1(input: &str) {
                 .map(|substring| {substring.parse::<i64>().unwrap()})
                 .collect::<Vec<i64>>();
 
-            // this only stores the last digit of every "step" until the line of zeroes
-            let mut output: Vec<i64> = Vec::new();
+            // this only stores the last digit of every 'step' until we reach a line of zeroes
+            let mut output_1: Vec<i64> = Vec::new();
+            // this only stores the first digit of every 'step' until we reach a line of zeroes
+            let mut output_2: Vec<i64> = Vec::new();
             'main:
             loop {
+                output_2.push(input[0].clone());
                 for index in 0..input.len()-1 {
                     input[index] = input[index+1] - input[index]
                 }
-                output.push(input.pop().unwrap());
+                output_1.push(input.pop().unwrap());
                 if input.iter().filter(|x| **x == 0).count() == input.len() {
                     break 'main;
                 }
             }
-            output.into_iter().fold(0_i64, |acc, x| {acc + x})
+            (
+                output_1.into_iter().fold(0_i64, |acc, x| {acc + x}),
+                output_2.into_iter().enumerate().fold(0_i64, |acc, (idx, x)| {acc + x * i64::pow(-1, idx as u32)})
+            )
         })
-        .fold(0, |acc, x| {acc + x});
+        .fold((0, 0), |acc, (x1, x2)| {(acc.0 + x1, acc.1 + x2)});
 
-    println!("{}", output);
+    println!("{}\n{}", output.0, output.1);
 }
