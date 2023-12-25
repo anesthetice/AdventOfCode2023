055a944f896925b947e23de1cec6534a67ecc149
anesthetice <elliot.currat@epfl.ch>
2023-12-26 00:57:55 +0100

    using the method of HyperNeutrino adapted to rust, also recursive but actually smart unlike my recursion, well I at least understand what they did so it isn't that bad ):<

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,107 +1,113 @@
-/// it's pretty clear I am just going to brute force this one ):<
-/// too complicated for my smooth brain
-use rayon::{iter::{self, ParallelIterator}, str::ParallelString};
+use rayon::{iter::{self, ParallelIterator, IntoParallelIterator}, str::ParallelString};
 
 fn main() {
-    let input: &str = include_str!("../input.txt");
+    let input: &'static str = include_str!("../input.txt");
 
-    let one: usize = input
+    
+    let answer_1: usize = input
         .par_lines()
         .map(|line| {
-            let (input, checklist) = parse_line(line);
-            enumerate(input)
-                .into_iter()
-                .filter(move |string| {
-                    check(string, &checklist)
-                })
-                .count()
+            let (input, checklist) = parse_line_part_1(line);
+            count(&input, &checklist)
         })
-        .fold(|| 0_usize, |acc, x| {acc + x})
+        .fold(|| 0_usize, |acc, x| {acc+x})
         .sum::<usize>();
 
-    println!("{}", one);
+    println!("{}", answer_1);
+
+    let answer_2: usize = input
+        .par_lines()
+        .map(|line| {
+            let (input, checklist) = parse_line_part_2(line);
+            count(&input, &checklist)
+        })
+        .fold(|| 0_usize, |acc, x| {acc+x})
+        .sum::<usize>();
+    
+    println!("{}", answer_2);
+
+    /*
+    let input: &str = "?#.??????#??#?#?#?#? 1,1,15";
+    let (input, checklist) = parse_line_part_1(input);
+    println!(
+        "{}",
+        count(&input, &checklist)
+    )  
+    */
+
 }
 
-fn parse_line(input: &str) -> (&str, Vec<u8>) {
+fn count(input: &str, checklist: &[usize]) -> usize {
+    if input.len() == 0 {
+        if checklist.len() == 0 {return 1}
+        else {return 0}
+    }
+
+    if checklist.len() == 0 {
+        if input.contains('#') {return 0}
+        else {return 1}
+    }
+
+    let mut result: usize = 0;
+
+    if ".?".contains(&input[0..1]) {
+        result += count(&input[1..], &checklist[..]);
+    }
+
+    if "#?".contains(&input[0..1]) {
+        // fist check that we even have the required length for a block of broken springs
+        if checklist[0] <= input.len()
+        // makes sure there aren't any working springs inside our block
+        && !input[0..checklist[0]].contains('.') 
+        // asserts that we've either reached the end of our input or that there isn't another broken spring at the end of our block as that would invalidate the block
+        && (input.len() == checklist[0] || &input[checklist[0]..checklist[0]+1] != "#")
+        {
+            result += count(&input[checklist[0]..], &checklist[1..])
+        }
+    }
+    result
+}
+
+fn parse_line_part_1(input: &str) -> (&str, Vec<usize>) {
     let (l_input, r_input) = input.split_once(' ').unwrap();
     (
         l_input,
-        r_input.split(',').map(|substr| {substr.parse::<u8>().unwrap()}).collect(),
+        r_input.split(',').map(|substr| {substr.parse::<usize>().unwrap()}).collect(),
     )
 }
 
-fn check(input: &str, checklist: &[u8]) -> bool {
-    checklist == input
-        .split('.')
-        .filter(|substr| {substr.len() > 0})
-        .map(|substr| {
-            substr.len() as u8
-        })
-        .collect::<Vec<u8>>()
+fn parse_line_part_2(input: &str) -> (String, Vec<usize>) {
+    let (l_input, r_input) = input.split_once(' ').unwrap();
+    let mut r_input: Vec<usize> = r_input.split(',').map(|substr| {substr.parse::<usize>().unwrap()}).collect();
+    let size = r_input.len();
+    r_input.extend_from_within(0..);
+    r_input.extend_from_within(0..);
+    r_input.extend_from_within(0..size);
+    (
+        format!("{}?{}?{}?{}?{}", l_input, l_input, l_input, l_input, l_input),
+        r_input,
+    )
 }
 
-fn enumerate(input: &str) -> Vec<String> {
-    let mut output: Vec<String> = Vec::new();
-    _enumerate_step(String::new(), input, &mut output);
-    output
-}
-
-fn _enumerate_step(current: String, remaining: &str, output: &mut Vec<String>) {
-    if let Some(subslice) = remaining.get(0..1) {
-        if subslice == "?" {
-            let mut alt_current_1: String = current.clone();
-            alt_current_1.push('#');
-            _enumerate_step(alt_current_1, &remaining[1..], output);
-
-            let mut alt_current_2: String = current.clone();
-            alt_current_2.push('.');
-            _enumerate_step(alt_current_2, &remaining[1..], output);
-        }
-        else {
-            let mut new_current: String = current.clone();
-            new_current.push_str(subslice);
-            _enumerate_step(new_current, &remaining[1..], output);
-        }
-    }
-    else {
-        output.push(current);
-    }
-}
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn parse_line_test() {
+    fn parse_line_part_1_test() {
         let input: &'static str = "???.### 1,1,3";
-        let output: (&'static str, Vec<u8>) = ("???.###", vec![1,1,3]);
+        let output: (&'static str, Vec<usize>) = ("???.###", vec![1,1,3]);
         assert_eq!(
             output,
-            crate::parse_line(input),
+            crate::parse_line_part_1(input),
         )
     }
     #[test]
-    fn check_test_1() {
-        let input: &'static str = "#.#.###";
-        let checklist: &[u8] = &[1,1,3];
-        assert!(crate::check(input, checklist))
-    }
-    #[test]
-    fn check_test_2() {
-        let input: &'static str = "..##.###";
-        let checklist: &[u8] = &[1,1,3];
-        assert!(!crate::check(input, checklist))
-    }
-    #[test]
-    fn enumerate_test() {
-        let input: &'static str = ".??.";
+    fn parse_line_part_2_test() {
+        let input: &'static str = ".# 1";
+        let output: (String, Vec<usize>) = (".#?.#?.#?.#?.#".to_string(), vec![1,1,1,1,1]);
         assert_eq!(
-            vec![
-                ".##.".to_string(),
-                ".#..".to_string(),
-                "..#.".to_string(),
-                "....".to_string(),
-            ],
-            crate::enumerate(input)
+            output,
+            crate::parse_line_part_2(input),
         )
     }
 }
