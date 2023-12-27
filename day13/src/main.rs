c290639634488b2f14565518ebdc7b50a3e118c2
anesthetice <elliot.currat@epfl.ch>
2023-12-27 12:55:31 +0100

    day13 part 1 works pretty neatly

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,9 +1,15 @@
-
 fn main() {
-    let a: Vec<String> = vec!["123".to_string(),"456".to_string(),"789".to_string()];
-    let b = transpose(a.as_slice());
-
-    println!("{:#?}\n\n{:#?}", a, b);
+    let input: &'static str = include_str!("../input.txt");
+    let input = parse_input(input);
+    
+    let answer = input
+        .into_iter()
+        .map(|block| {
+            let block_t = transpose(block.as_slice());
+            op(block.as_slice()) * 100 + op(block_t.as_slice())
+        })
+        .fold(0_usize,  |acc, x| {acc+x});
+    println!("{}", answer);
 }
 
 fn parse_input(input: &str) -> Vec<Vec<String>>{
@@ -20,12 +26,37 @@ fn transpose(input: &[String]) -> Vec<String> {
     for _ in 0..input[0].len() {
         output.push(String::new());
     }
-
-    for (i, string) in input.iter().enumerate() {
+    for string in input.iter() {
     for (j, chr) in string.char_indices() {
         output[j].push(chr)
     }
     }
-
     output
+}
+
+fn op(input: &[String]) -> usize {
+    let idx_step = |curr_idxs: Option<(usize, usize)>, init_idx: usize, length: usize| -> Option<(usize, usize)> {
+        match curr_idxs {
+            Some((a, b)) => {
+                if a > 0 && b < length-1 {
+                    Some((a-1, b+1))
+                }
+                else {
+                    None
+                }
+            },
+            None => {Some((init_idx-1, init_idx))}
+        }
+    };
+    let mut sum: usize = 0;
+    for idx in 1..input.len() {
+        let mut valid: bool = true;
+        let mut curr_idxs: Option<(usize, usize)> = None;
+        while let Some((a, b)) = idx_step(curr_idxs, idx, input.len()) {
+            if input[a] != input[b] {valid=false; break;}
+            curr_idxs = Some((a, b));
+        }
+        if valid {sum+=idx}
+    }
+    sum
 }
